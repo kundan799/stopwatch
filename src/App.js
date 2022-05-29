528bbfa914fb0b1e30c1e0143e48d9f5aa50726c
Kundan Kumar Keshri <101567147+kundan799@users.noreply.github.com>
2022-05-29 12:03:10 +0530

    add watch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+import StopWatch from "./components/StopWatch";
+
+const App = () => {
+  return (
+    <div className="App" style={{textAlign:"center"}} >
+      <StopWatch />
+    </div>
+  );
+};
+
+export default App;
